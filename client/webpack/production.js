8538df001a453e6d390f2bd0d80fe5dd3239fcb2
Meuterer1 <kamila.dziuba@o2.pl>
2023-11-14 18:44:34 +0100

    update webpack

--- client/webpack/production.js
+++ client/webpack/production.js
@@ -1,32 +1,35 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+//const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   optimization: {
-    minimizer: [new OptimizeCssAssetsWebpackPlugin({})],
+    //minimizer: [new OptimizeCssAssetsWebpackPlugin({})],
+    minimizer: [new CssMinimizerPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.module\.(s(a|c)ss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              modules: {
-                localIdentName: "[local",
-              },
-            },
-          },
-          {
-            loader: "saas-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
+        // use: [
+        //   MiniCssExtractPlugin.loader,
+        //   {
+        //     loader: "css-loader",
+        //     options: {
+        //       modules: {
+        //         localIdentName: "[local",
+        //       },
+        //     },
+        //   },
+        //   {
+        //     loader: "saas-loader",
+        //     options: {
+        //       sourceMap: true,
+        //     },
+        //   },
+        // ],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
       {
         test: /\.(s(a|c)ss)$/,
@@ -46,9 +49,10 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: "style.[contenthash:6].css",
-      chunkFilename: "style.[contenthash:6].css",
-    }),
+    // new MiniCssExtractPlugin({
+    //   filename: "style.[contenthash:6].css",
+    //   chunkFilename: "style.[contenthash:6].css",
+    // }),
+    new MiniCssExtractPlugin(),
   ],
 };
