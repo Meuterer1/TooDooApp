f93f01b568d99b4b73ee9e6455101c6683487be5
Meuterer1 <kamila.dziuba@o2.pl>
2023-11-19 14:08:19 +0100

    build client

--- client/build/index.dec183.js
+++ client/build/index.dec183.js
@@ -29744,7 +29744,7 @@ var useSynchronise = function useSynchronise() {
           case 0:
             _context.prev = 0;
             _context.next = 3;
-            return lib_axios.get("https://stark-earth-03918-bae1ddc364db.herokuapp.com/");
+            return lib_axios.get("https://meuterertodoappserver-110f55b64ca3.herokuapp.com/");
           case 3:
             response = _context.sent;
             console.log("useSynchronise response: ", response.data);
